3eca8783a227abd2b2faf0803d3aa02836f51a8b
BeskarDev <60790042+BeskarDev@users.noreply.github.com>
2024-04-03 18:30:26 +0000

    optimize design of cards

--- src/features/CombatArts/PlayingCard.tsx
+++ src/features/CombatArts/PlayingCard.tsx
@@ -6,13 +6,17 @@ import { Box, Card, Divider, Typography } from '@mui/material';
 export const PlayingCard: React.FC<CombatArt> = ({ name, weapons, effect }) => {
   return (
     <div className="combat-art--card">
-      <Box sx={{ flexGrow: 1 }}>
-        <Typography variant="h6" fontWeight="bold">{name}</Typography>
-        <Typography variant="caption">{weapons}</Typography>
-        <Divider sx={{ mb: 1 }} />
-        <Typography variant="body1">{parse(effect)}</Typography>
-      </Box>
-      <Typography variant="caption" sx={{ ml: 'auto' }}>©{new Date().getFullYear()} Nexus RPG</Typography>
+      <div className="combat-art--card__inner">
+        <Box sx={{ flexGrow: 1 }}>
+          <Typography variant="h5" fontWeight="bold">{name}</Typography>
+          <Typography variant="caption">{weapons}</Typography>
+          <Divider sx={{ mb: 1 }} />
+          <Typography className={effect.length >= 600 ? 'small-text' : ''} variant="body1">{parse(effect)}</Typography>
+        </Box>
+        <Typography variant="caption" sx={{ ml: 'auto', mr: -1, mb: -1, fontSize: '6px !important' }}>
+          ©{new Date().getFullYear()} Nexus RPG
+        </Typography>
+      </div>
     </div>
   );
 };
