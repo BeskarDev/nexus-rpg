63a47a9f01db4ca94d78af8fe51b2a2ae5ccb215
BeskarDev <60790042+BeskarDev@users.noreply.github.com>
2024-06-28 20:32:03 +0000

    add shared notes

---
+++ src/features/CharacterSheet/CharacterSheetTabs/05_SharedNotes/SharedNotes.tsx
@@ -0,0 +1,127 @@
+import { useColorMode } from '@docusaurus/theme-common'
+import { Save } from '@mui/icons-material'
+import {
+	Box,
+	CircularProgress,
+	IconButton,
+	TextField,
+	Typography,
+} from '@mui/material'
+import { db } from '@site/src/config/firebase'
+import { useAuth } from '@site/src/hooks/firebaseAuthContext'
+import {
+	collection,
+	getDocs,
+	query,
+	updateDoc,
+	where,
+} from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { SectionHeader } from '../../CharacterSheet'
+
+export const SharedNotes: React.FC = () => {
+	const { currentUser } = useAuth()
+	const { colorMode } = useColorMode()
+	const [ref, setRef] = useState(undefined)
+
+	const [unsavedChanges, setUnsavedChanges] = useState(false)
+	const [loadingSave, setLoadingSave] = useState(false)
+	const [notes, setNotes] = useState('')
+
+	const [isLoading, setIsLoading] = useState(true)
+	const [error, setError] = useState(null)
+
+	useEffect(() => {
+		const fetchData = async () => {
+			if (!currentUser) {
+				setError('User not logged in')
+				setIsLoading(false)
+				return
+			}
+
+			try {
+				const q = query(
+					collection(db, 'shared-notes'),
+					where('allowedUsers', 'array-contains', currentUser.uid),
+				)
+				const querySnapshot = await getDocs(q)
+
+				if (querySnapshot.empty) {
+					setError('No shared notes found')
+				} else {
+					setRef(querySnapshot.docs[0].ref)
+					const data = querySnapshot.docs[0].data()
+					setNotes(data.notes)
+				}
+			} catch (err) {
+				setError(err.message)
+			} finally {
+				setIsLoading(false)
+			}
+		}
+
+		fetchData()
+	}, [currentUser])
+
+	if (isLoading) return <p>Loading...</p>
+	if (error) return <p>Error: {error}</p>
+
+	const updateNotes = (notes: string) => {
+		setNotes(notes)
+		setUnsavedChanges(true)
+	}
+
+	const saveNotes = async () => {
+		console.log('about to save shared notes...')
+		if (unsavedChanges) {
+			setLoadingSave(true)
+			await updateDoc(ref, { notes })
+			setUnsavedChanges(false)
+			setLoadingSave(false)
+		}
+		console.log('done saving shared notes')
+	}
+
+	return (
+		<>
+			<Box
+				sx={{
+					position: 'sticky',
+					top: '165px',
+					zIndex: 100,
+					backgroundColor:
+						colorMode === 'dark' ? 'var(--ifm-background-color)' : 'white',
+					display: 'flex',
+					alignItems: 'center',
+					gap: 1,
+					width: '100%',
+				}}
+			>
+				<SectionHeader>Shared Notes</SectionHeader>
+				<IconButton
+					disabled={!unsavedChanges}
+					onClick={saveNotes}
+					sx={{ mb: '6px' }}
+				>
+					{loadingSave ? <CircularProgress size={20} /> : <Save />}
+				</IconButton>
+			</Box>
+			<Typography
+				variant="caption"
+				sx={{
+					mb: 1,
+				}}
+			>
+				shared across multiple users. make sure to not edit at the same time!
+			</Typography>
+			<TextField
+				multiline
+				minRows={20}
+				maxRows={20}
+				value={notes}
+				onChange={(event) => updateNotes(event.target.value)}
+				sx={{ maxWidth: '100%' }}
+			/>
+		</>
+	)
+}
