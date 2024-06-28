b77c1ee82dd48163664c60f96b5517f9965f91c0
BeskarDev <60790042+BeskarDev@users.noreply.github.com>
2024-06-28 14:18:21 +0000

    finish items tab

---
+++ src/features/CharacterSheet/CharacterSheetTabs/EquipmentRow.tsx
@@ -0,0 +1,87 @@
+import { Box, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
+
+import { AttributeField } from '../CharacterSheet'
+import { Equipment } from '../types/Character'
+
+export type EquipmentRowProps = {
+	label: string
+	equipment: Equipment
+	updateEquipment: (update: Partial<Equipment>) => void
+}
+
+export const EquipmentRow: React.FC<EquipmentRowProps> = ({
+	label,
+	equipment: initialEquipment,
+	updateEquipment,
+}) => {
+	const [equipment, setEquipment] = useState<Equipment>(initialEquipment)
+
+	return (
+		<>
+			<Typography variant="body2" fontWeight="bold">
+				{label}
+			</Typography>
+			<Box
+				sx={{
+					display: 'flex',
+					alignItems: 'center',
+					flexWrap: 'wrap',
+					pb: 1,
+					borderBottom: 1,
+					borderColor: 'divider',
+				}}
+			>
+				<TextField
+					variant="standard"
+					value={equipment.name}
+					onChange={(event) =>
+						setEquipment((w) => ({ ...w, name: event.target.value }))
+					}
+					onBlur={() => updateEquipment({ name: equipment.name })}
+					label="Name"
+					sx={{ maxWidth: '12rem', flexGrow: 1 }}
+				/>
+				<TextField
+					variant="standard"
+					multiline
+					minRows={1}
+					maxRows={1}
+					value={equipment.properties}
+					onChange={(event) =>
+						setEquipment((w) => ({ ...w, properties: event.target.value }))
+					}
+					onBlur={() => updateEquipment({ properties: equipment.properties })}
+					label="Properties"
+					sx={{ maxWidth: '16rem', flexGrow: 1 }}
+					inputProps={{ sx: { fontSize: 12 } }}
+				/>
+				<AttributeField
+					type="number"
+					size="small"
+					value={initialEquipment.cost}
+					onChange={(event) =>
+						updateEquipment({ cost: Number(event.target.value) })
+					}
+					label="Cost"
+					sx={{ maxWidth: '8rem', flexGrow: 0 }}
+					inputProps={{
+						sx: {
+							textAlign: 'right',
+						},
+					}}
+				/>
+				<AttributeField
+					type="number"
+					size="small"
+					value={initialEquipment.load}
+					onChange={(event) =>
+						updateEquipment({ load: Number(event.target.value) })
+					}
+					label="Load"
+					sx={{ maxWidth: '4rem', flexGrow: 0 }}
+				/>
+			</Box>
+		</>
+	)
+}
--- src/features/CharacterSheet/CharacterSheetTabs/ItemsTab.tsx
+++ src/features/CharacterSheet/CharacterSheetTabs/ItemsTab.tsx
@@ -4,8 +4,10 @@ import React, { useMemo } from 'react'
 import { AddCircle, HelpOutline } from '@mui/icons-material'
 import { AttributeField, SectionHeader } from '../CharacterSheet'
 import { DeepPartial } from '../CharacterSheetContainer'
-import { Character, Weapon } from '../types/Character'
+import { Character, Item, Weapon } from '../types/Character'
 
+import { EquipmentRow } from './EquipmentRow'
+import { ItemRow } from './ItemRow'
 import { WeaponRow } from './WeaponRow'
 
 export type ItemsTabProps = {
@@ -24,10 +26,16 @@ export const ItemsTab: React.FC<ItemsTabProps> = ({
 			.map((w) => w.load)
 			.reduce((partialSum, a) => partialSum + a, 0)
 		const equipmentLoad = Object.keys(equipment)
-			.map((k) => Number(equipment[k].load))
+			.map((k) =>
+				k !== 'rings'
+					? Number(equipment[k].load)
+					: equipment[k]
+							.map((r) => r.load)
+							.reduce((partialSum, a) => partialSum + a, 0),
+			)
 			.reduce((partialSum, a) => partialSum + a, 0)
 		const itemLoad = items
-			.map((i) => i.load)
+			.map((i) => i.load * i.amount)
 			.reduce((partialSum, a) => partialSum + a, 0)
 		return weaponLoad + equipmentLoad + itemLoad
 	}, [character])
@@ -59,6 +67,35 @@ export const ItemsTab: React.FC<ItemsTabProps> = ({
 			items: { coins, encumbrance, weapons: newWeapons, equipment, items },
 		})
 		weapons.pop()
+	}
+
+	const addNewItem = () => {
+		items.push({
+			name: 'new item',
+			properties: '',
+			cost: 0,
+			load: 0,
+			amount: 0,
+		})
+		updateCharacter({
+			items: { coins, encumbrance, weapons, equipment, items },
+		})
+	}
+
+	const updateItem = (update: Partial<Item>, index: number) => {
+		const newItems = [...items]
+		newItems[index] = { ...items[index], ...update }
+		return updateCharacter({
+			items: { items: newItems },
+		})
+	}
+
+	const deleteItem = (item: Item) => {
+		const newItems = [...items].filter((s) => s != item)
+		updateCharacter({
+			items: { coins, encumbrance, weapons, equipment, items: newItems },
+		})
+		items.pop()
 	}
 
 	return (
@@ -162,6 +199,9 @@ export const ItemsTab: React.FC<ItemsTabProps> = ({
 					/>
 				</Box>
 			</Box>
+
+			<Box sx={{ width: '100%', flexGrow: 1 }} />
+
 			<Box sx={{ mb: 2 }}>
 				<Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
 					<SectionHeader>Weapons</SectionHeader>
@@ -188,6 +228,161 @@ export const ItemsTab: React.FC<ItemsTabProps> = ({
 							weapon={w}
 							updateWeapon={(update) => updateWeapon(update, index)}
 							deleteWeapon={() => deleteWeapon(w)}
+						/>
+					))}
+				</Box>
+			</Box>
+
+			<Box sx={{ width: '100%', flexGrow: 1 }} />
+
+			<Box sx={{ mb: 2, maxWidth: '28rem' }}>
+				<SectionHeader>Equipment</SectionHeader>
+				<Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+					<EquipmentRow
+						label="Head"
+						equipment={equipment.head}
+						updateEquipment={(update) =>
+							updateCharacter({
+								items: {
+									equipment: { head: { ...equipment.head, ...update } },
+								},
+							})
+						}
+					/>
+					<EquipmentRow
+						label="Neck"
+						equipment={equipment.neck}
+						updateEquipment={(update) =>
+							updateCharacter({
+								items: {
+									equipment: { neck: { ...equipment.neck, ...update } },
+								},
+							})
+						}
+					/>
+					<EquipmentRow
+						label="Back"
+						equipment={equipment.back}
+						updateEquipment={(update) =>
+							updateCharacter({
+								items: {
+									equipment: { neck: { ...equipment.neck, ...update } },
+								},
+							})
+						}
+					/>
+					<EquipmentRow
+						label="Body"
+						equipment={equipment.body}
+						updateEquipment={(update) =>
+							updateCharacter({
+								items: {
+									equipment: { body: { ...equipment.body, ...update } },
+								},
+							})
+						}
+					/>
+					<EquipmentRow
+						label="Hands"
+						equipment={equipment.hands}
+						updateEquipment={(update) =>
+							updateCharacter({
+								items: {
+									equipment: { hands: { ...equipment.hands, ...update } },
+								},
+							})
+						}
+					/>
+					<EquipmentRow
+						label="Ring 1"
+						equipment={equipment.rings[0]}
+						updateEquipment={(update) =>
+							updateCharacter({
+								items: {
+									equipment: {
+										rings: [
+											{ ...equipment.rings[0], ...update },
+											equipment.rings[1],
+											equipment.rings[2],
+										],
+									},
+								},
+							})
+						}
+					/>
+					<EquipmentRow
+						label="Ring 2"
+						equipment={equipment.rings[1]}
+						updateEquipment={(update) =>
+							updateCharacter({
+								items: {
+									equipment: {
+										rings: [
+											equipment.rings[0],
+											{ ...equipment.rings[1], ...update },
+											equipment.rings[2],
+										],
+									},
+								},
+							})
+						}
+					/>
+					<EquipmentRow
+						label="Ring 3"
+						equipment={equipment.rings[2]}
+						updateEquipment={(update) =>
+							updateCharacter({
+								items: {
+									equipment: {
+										rings: [
+											equipment.rings[0],
+											equipment.rings[1],
+											{ ...equipment.rings[2], ...update },
+										],
+									},
+								},
+							})
+						}
+					/>
+					<EquipmentRow
+						label="Waist"
+						equipment={equipment.waist}
+						updateEquipment={(update) =>
+							updateCharacter({
+								items: {
+									equipment: { waist: { ...equipment.waist, ...update } },
+								},
+							})
+						}
+					/>
+					<EquipmentRow
+						label="Feet"
+						equipment={equipment.feet}
+						updateEquipment={(update) =>
+							updateCharacter({
+								items: {
+									equipment: { feet: { ...equipment.feet, ...update } },
+								},
+							})
+						}
+					/>
+				</Box>
+			</Box>
+
+			<Box sx={{ mb: 2 }}>
+				<Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+					<SectionHeader>Items</SectionHeader>
+					<IconButton onClick={addNewItem} sx={{ mb: 0.75 }}>
+						<AddCircle />
+					</IconButton>
+				</Box>
+				<Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+					{items.map((i, index) => (
+						<ItemRow
+							key={i.name + index}
+							item={i}
+							updateItem={(update) => updateItem(update, index)}
+							deleteItem={() => deleteItem(i)}
 						/>
 					))}
 				</Box>
