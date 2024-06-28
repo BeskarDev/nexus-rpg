9002889da51009581192987f500037bab61254a1
BeskarDev <60790042+BeskarDev@users.noreply.github.com>
2024-06-28 13:40:37 +0000

    add coins and encumbrance

--- src/features/CharacterSheet/CharacterSheetTabs/ItemsTab.tsx
+++ src/features/CharacterSheet/CharacterSheetTabs/ItemsTab.tsx
@@ -1,8 +1,8 @@
 import { Box, IconButton, Tooltip } from '@mui/material'
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import { AddCircle, HelpOutline } from '@mui/icons-material'
-import { SectionHeader } from '../CharacterSheet'
+import { AttributeField, SectionHeader } from '../CharacterSheet'
 import { DeepPartial } from '../CharacterSheetContainer'
 import { Character, Weapon } from '../types/Character'
 
@@ -18,6 +18,19 @@ export const ItemsTab: React.FC<ItemsTabProps> = ({
 	updateCharacter,
 }) => {
 	const { coins, encumbrance, weapons, equipment, items } = character.items
+
+	const currentLoad: number = useMemo(() => {
+		const weaponLoad = weapons
+			.map((w) => w.load)
+			.reduce((partialSum, a) => partialSum + a, 0)
+		const equipmentLoad = Object.keys(equipment)
+			.map((k) => Number(equipment[k].load))
+			.reduce((partialSum, a) => partialSum + a, 0)
+		const itemLoad = items
+			.map((i) => i.load)
+			.reduce((partialSum, a) => partialSum + a, 0)
+		return weaponLoad + equipmentLoad + itemLoad
+	}, [character])
 
 	const addNewWeapon = () => {
 		weapons.push({
@@ -57,6 +70,98 @@ export const ItemsTab: React.FC<ItemsTabProps> = ({
 				justifyContent: 'center',
 			}}
 		>
+			<Box>
+				<Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+					<SectionHeader>Coins & Encumbrance</SectionHeader>
+					<Tooltip
+						title={
+							<>
+								<b>Encumbered.</b> You suffer +1 bane on Strength or Agility
+								rolls for movement, such as climbing or swimming, and during
+								travel.
+								<br />
+								<b>Over-Encumbered.</b> You can‘t move. You can‘t use any
+								Actions or do any rolls for physical activity.
+							</>
+						}
+					>
+						<HelpOutline fontSize="small" sx={{ mb: 0.75 }} />
+					</Tooltip>
+				</Box>
+				<Box sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap' }}>
+					<AttributeField
+						type="number"
+						size="medium"
+						value={coins}
+						onChange={(event) =>
+							updateCharacter({ items: { coins: Number(event.target.value) } })
+						}
+						label="Coins"
+						sx={{
+							mr: 1,
+							maxWidth: '12rem',
+							'& .MuiOutlinedInput-root': {
+								'& .MuiOutlinedInput-notchedOutline': {
+									borderWidth: '2px',
+								},
+							},
+						}}
+						inputProps={{
+							sx: {
+								textAlign: 'right',
+							},
+						}}
+					/>
+					<AttributeField
+						size="medium"
+						value={currentLoad}
+						label="Current Load"
+						sx={{
+							mr: 1,
+							maxWidth: '7rem',
+							'& .MuiOutlinedInput-root': {
+								'& .MuiOutlinedInput-notchedOutline': {
+									borderWidth: '2px',
+								},
+							},
+						}}
+					/>
+					<AttributeField
+						type="number"
+						size="small"
+						value={encumbrance.encumberedAt}
+						onChange={(event) =>
+							updateCharacter({
+								items: {
+									encumbrance: { encumberedAt: Number(event.target.value) },
+								},
+							})
+						}
+						label="Encumbered"
+						helperText="2 * Strength"
+						sx={{
+							maxWidth: '6.5rem',
+						}}
+					/>
+					<AttributeField
+						type="number"
+						size="small"
+						value={encumbrance.overencumberedAt}
+						onChange={(event) =>
+							updateCharacter({
+								items: {
+									encumbrance: { encumberedAt: Number(event.target.value) },
+								},
+							})
+						}
+						label="Overencumbered"
+						helperText="3 * Strength"
+						sx={{
+							maxWidth: '7.5rem',
+						}}
+					/>
+				</Box>
+			</Box>
 			<Box sx={{ mb: 2 }}>
 				<Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
 					<SectionHeader>Weapons</SectionHeader>
