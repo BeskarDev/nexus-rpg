9040d1390e7369036be178685f73c5d6f7b255b1
BeskarDev <60790042+BeskarDev@users.noreply.github.com>
2024-07-01 15:53:01 +0000

    add testing state of draggable for skills

--- src/components/DynamicList/DynamicList.tsx
+++ src/components/DynamicList/DynamicList.tsx
@@ -1,30 +1,27 @@
-import { List } from '@mui/material'
+import { List, ListProps } from '@mui/material'
 import React from 'react'
 import {
 	DragDropContext,
 	Droppable,
 	OnDragEndResponder,
 } from 'react-beautiful-dnd'
-import { DynamicListItem } from './DynamicListItem'
-
-export type Item = {}
 
 export type DynamicListProps = {
-	items: Item[]
 	onDragEnd: OnDragEndResponder
-}
+} & ListProps
 
 export const DynamicList: React.FC<DynamicListProps> = React.memo(
-	({ items, onDragEnd }) => {
+	({ onDragEnd, children, ...props }) => {
 		return (
 			<DragDropContext onDragEnd={onDragEnd}>
 				<Droppable droppableId="droppable-list">
 					{(provided) => (
-						<List ref={provided.innerRef} {...provided.droppableProps}>
-							{items.map((item: Item, index: number) => (
-								<DynamicListItem item={item} index={index} key={item.id} />
-							))}
-							{provided.placeholder}
+						<List
+							ref={provided.innerRef}
+							{...provided.droppableProps}
+							{...props}
+						>
+							{children}
 						</List>
 					)}
 				</Droppable>
---
+++ src/features/CharacterSheet/utils/updateAllFromCollection.ts
@@ -0,0 +1,25 @@
+// function to be used when data structure changes and needs to be migrated in firestore
+/* const updateAllFromCollection = async (collectionName) => {
+	const col = collection(db, collectionName)
+
+	const newDocumentBody = {
+		message: 'hello world',
+	}
+
+	col
+		.where('message', '==', 'goodbye world')
+		.get()
+		.then((response) => {
+			let batch = firebase.firestore().batch()
+			response.docs.forEach((doc) => {
+				const docRef = firebase
+					.firestore()
+					.collection(collectionName)
+					.doc(doc.id)
+				batch.update(docRef, newDocumentBody)
+			})
+			batch.commit().then(() => {
+				console.log(`updated all documents inside ${collectionName}`)
+			})
+		})
+} */
