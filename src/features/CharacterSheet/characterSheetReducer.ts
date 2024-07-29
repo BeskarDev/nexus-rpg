75769f6bb6e3ac71db816d80dccdf737fcf37d17
BeskarDev <60790042+BeskarDev@users.noreply.github.com>
2024-07-29 07:34:24 +0000

    update list functions for skills tab

--- src/features/CharacterSheet/characterSheetReducer.ts
+++ src/features/CharacterSheet/characterSheetReducer.ts
@@ -1,8 +1,30 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { deepCopy } from '@site/src/components/DynamicList/utils'
-import { CharacterDocument } from '@site/src/types/Character'
+import { Ability, CharacterDocument, Skill } from '@site/src/types/Character'
 import { Character } from './../../types/Character'
 import { DeepPartial } from './CharacterSheetContainer'
+
+function isObject(value: any) {
+	return value !== null && typeof value === 'object'
+}
+
+function mergeDeep(target: any, source: any) {
+	if (isObject(target) && isObject(source)) {
+		for (const key in source) {
+			if (isObject(source[key])) {
+				// Recursive call for nested objects
+				target[key] = mergeDeep(target[key] || {}, source[key])
+			} else {
+				// Update value for non-objects
+				target[key] = source[key]
+			}
+		}
+	} else {
+		// Replace entire value if not objects
+		return source
+	}
+	return target
+}
 
 export type CharacterSheetReducerState = {
 	activeCharacter?: CharacterDocument
@@ -50,30 +72,56 @@ export const {
 				docRef: state.activeCharacter.docRef,
 			}
 
-			function isObject(value: any) {
-				return value !== null && typeof value === 'object'
-			}
-
-			function mergeDeep(target: any, source: any) {
-				if (isObject(target) && isObject(source)) {
-					for (const key in source) {
-						if (isObject(source[key])) {
-							// Recursive call for nested objects
-							target[key] = mergeDeep(target[key] || {}, source[key])
-						} else {
-							// Update value for non-objects
-							target[key] = source[key]
-						}
-					}
-				} else {
-					// Replace entire value if not objects
-					return source
-				}
-				return target
-			}
-
 			state.unsavedChanges = true
 			state.activeCharacter = mergeDeep(newCharacter, copiedUpdate)
+		},
+		addNewSkill: (state) => {
+			state.activeCharacter.skills.skills.splice(0, 0, {
+				id: crypto.randomUUID(),
+				name: 'new skill',
+				rank: 0,
+				xp: 0,
+			})
+		},
+		updateSkill: (
+			state,
+			action: PayloadAction<{ update: Partial<Skill>; index: number }>,
+		) => {
+			const update = action.payload.update
+			const index = action.payload.index
+			state.activeCharacter.skills.skills[index] = {
+				...state.activeCharacter.skills.skills[index],
+				...update,
+			}
+		},
+		deleteSkill: (state, action: PayloadAction<Skill>) => {
+			state.activeCharacter.skills.skills =
+				state.activeCharacter.skills.skills.filter(
+					(s) => s.id !== action.payload.id,
+				)
+		},
+		addNewAbility: (state) => {
+			state.activeCharacter.skills.abilities.splice(0, 0, {
+				id: crypto.randomUUID(),
+				description: '',
+			})
+		},
+		updateAbility: (
+			state,
+			action: PayloadAction<{ update: string; index: number }>,
+		) => {
+			const update = action.payload.update
+			const index = action.payload.index
+			state.activeCharacter.skills.abilities[index] = {
+				...state.activeCharacter.skills.abilities[index],
+				description: update,
+			}
+		},
+		deleteAbility: (state, action: PayloadAction<Ability>) => {
+			state.activeCharacter.skills.abilities =
+				state.activeCharacter.skills.abilities.filter(
+					(s) => s.id !== action.payload.id,
+				)
 		},
 	},
 })
