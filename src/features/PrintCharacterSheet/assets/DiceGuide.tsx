6ac79ac0abffd3ae33168d56590b755a38b81aea
BeskarDev <60790042+BeskarDev@users.noreply.github.com>
2024-09-24 18:04:23 +0000

    finish statistics sheet

--- src/features/PrintCharacterSheet/assets/DiceGuide.tsx
+++ src/features/PrintCharacterSheet/assets/DiceGuide.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const DiceGuide: React.FC = () => (
 	<svg
-		width="29"
-		height="195"
+		width="34.8"
+		height="234"
 		viewBox="0 0 29 195"
 		fill="none"
 		xmlns="http://www.w3.org/2000/svg"
