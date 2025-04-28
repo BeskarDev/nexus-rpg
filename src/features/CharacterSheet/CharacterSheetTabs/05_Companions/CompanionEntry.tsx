0fb83c6cd952935985114e62418e259f70e8993a
BeskarDev <60790042+BeskarDev@users.noreply.github.com>
2025-04-28 13:05:18 +0000

    make companion entry as accordion

--- src/features/CharacterSheet/CharacterSheetTabs/05_Companions/CompanionEntry.tsx
+++ src/features/CharacterSheet/CharacterSheetTabs/05_Companions/CompanionEntry.tsx
@@ -1,14 +1,27 @@
 import { Delete } from '@mui/icons-material'
-import { Box, TextField, Tooltip, IconButton } from '@mui/material'
-import { CharacterDocument, Companion } from '@site/src/types/Character'
-import { DeepPartial } from '../../CharacterSheetContainer'
-import { characterSheetActions } from '../../characterSheetReducer'
+import {
+	Accordion,
+	AccordionSummary,
+	AccordionDetails,
+	Box,
+	TextField,
+	Tooltip,
+	IconButton,
+	Typography,
+} from '@mui/material'
+import { ExpandMore } from '@mui/icons-material'
+import { Companion } from '@site/src/types/Character'
 import { useAppDispatch } from '../../hooks/useAppDispatch'
+import { characterSheetActions } from '../../characterSheetReducer'
+import { useState } from 'react'
 
-export const CompanionEntry: React.FC<{ companion: Companion, index: number, showControls: boolean }> = ({
-	companion, index, showControls,
-}) => {
+export const CompanionEntry: React.FC<{
+	companion: Companion
+	index: number
+	showControls: boolean
+}> = ({ companion, index, showControls }) => {
 	const dispatch = useAppDispatch()
+	const [expanded, setExpanded] = useState(false)
 
 	const updateCompanion = (update: Partial<Companion>, index: number) => {
 		dispatch(characterSheetActions.updateCompanion({ update, index }))
@@ -19,43 +32,65 @@ export const CompanionEntry: React.FC<{ companion: Companion, index: number, sho
 	}
 
 	return (
-		<Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-			<TextField
-				variant="standard"
-				value={companion.name}
-				onChange={(event) =>
-					updateCompanion({ name: event.target.value }, index)
-				}
-				label="Name"
-				sx={{ maxWidth: '15rem' }}
-			/>
-			<TextField
-				variant="standard"
-				value={companion.statistics.health.current}
-				onChange={(event) =>
-					updateCompanion(
-						{
-							statistics: {
-								...companion.statistics,
-								health: {
-									...companion.statistics.health,
-									current: Number(event.target.value),
+		<Accordion
+			expanded={expanded}
+			disableGutters
+			sx={{ flexGrow: 1, maxWidth: '47rem', mt: 0 }}
+		>
+			<AccordionSummary
+				expandIcon={<ExpandMore onClick={() => setExpanded(!expanded)} />}
+				sx={{
+					gap: 1,
+					pt: 0,
+					px: 0,
+					flexDirection: 'row-reverse',
+					'& .MuiAccordionSummary-content': {
+						display: 'block',
+					},
+				}}
+			>
+				<Typography>{companion.name || 'Unnamed Companion'}</Typography>
+			</AccordionSummary>
+			<AccordionDetails>
+				<Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+					<TextField
+						variant="standard"
+						value={companion.name}
+						onChange={(event) =>
+							updateCompanion({ name: event.target.value }, index)
+						}
+						label="Name"
+						sx={{ maxWidth: '15rem' }}
+					/>
+					<TextField
+						variant="standard"
+						value={companion.statistics.health.current}
+						onChange={(event) =>
+							updateCompanion(
+								{
+									statistics: {
+										...companion.statistics,
+										health: {
+											...companion.statistics.health,
+											current: Number(event.target.value),
+										},
+									},
 								},
-							},
-						},
-						index,
-					)
-				}
-				label="Health"
-				sx={{ maxWidth: '10rem' }}
-			/>
-			{showControls && (
-				<Tooltip title="Delete Companion">
-					<IconButton onClick={() => deleteCompanion(index)}>
-						<Delete />
-					</IconButton>
-				</Tooltip>
-			)}
-		</Box>
+								index,
+							)
+						}
+						label="Health"
+						sx={{ maxWidth: '10rem' }}
+					/>
+					{showControls && (
+						<Tooltip title="Delete Companion">
+							<IconButton onClick={() => deleteCompanion(index)}>
+								<Delete />
+							</IconButton>
+						</Tooltip>
+					)}
+				</Box>
+			</AccordionDetails>
+		</Accordion>
 	)
 }
